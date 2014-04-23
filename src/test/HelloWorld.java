30297341d08ae735b8722f67a3a75d2f7a41d11e
jiangyunlong <jyl0401@163.com>
2014-04-23 20:36:41 +0800

    test2

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,6 +3,8 @@ package test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		String str = "Hello World!";
+		System.out.println(str);
 	}
 }
